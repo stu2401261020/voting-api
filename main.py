50e64b373301c9a5b089011debaa542f47f59bfa
stu2401261020 <stu2401261020@uni-plovdiv.bg>
2024-11-23 15:21:54 +0200

    Implement basic get function

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
 
 app = FastAPI()
 
+# Define the database URL and create the SQLAlchemy engine
+database_url = os.environ.get('DATABASE_URL')
+engine = create_engine(database_url)
+# Create a session factory using the engine
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 @app.get("/")
 async def root():
@@ -12,3 +21,13 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/votes/{voter_id}")
+async def get_votes(voter_id: string):
+    # Create a new session
+    db = SessionLocal()
+    # Retrieve the user from the database using the user_id
+    vote = db.query(vote_registrations).filter(vote_registrations.vote_registration_id == voter_id).first()
+    # Close the session
+    db.close()
+    return {"vote": vote}
